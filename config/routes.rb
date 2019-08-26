5ec7df7fa61ae57a0bf9756dc92fb82682b00d95
SwanHub <jdprince555@gmail.com>
2019-08-26 00:12:45 -0600

    landing page recalibrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'users#home'
 
   resources :users, only: [:index, :show, :new, :create]
   get '/home', to: 'users#home', as: 'home'
